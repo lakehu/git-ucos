3d8a384965b1dfbb070ae4dc6aab963e7ab0a7e4
Lake Hu <Lake.hu@outlook.com>
2024-01-19 21:02:46 +0800

    Fix stack issue of Linux sample demo

--- makefile
+++ makefile
@@ -16,8 +16,7 @@
 DEBUGGING = -ggdb
 CC=gcc
 #CFLAGS += -I./ $(DEBUGGING) -m32
-CFLAGS += -I./ -I/usr/include $(DEBUGGING)
-#CFLAGS += -I./ -I/usr/include $(DEBUGGING)  -fno-stack-protector 
+CFLAGS += -I./ -I/usr/include $(DEBUGGING) 
 objects := os_cpu_c.o ucos_ii.o sample.o
 sample:$(objects)
 	cc -o sample $(objects)  -lpthread
--- sample.c
+++ sample.c
@@ -54,13 +54,19 @@ int main (void)
 	char sTask4[] = "Task 4";
 	char sTask5[] = "Task 5";
 
+	printf("OSMinStkSize=%d\n",OSMinStkSize());
 	OSInit();
 
-	OSTaskCreate( MyTask, sTask1, (void*)Stk1, 4 );
-	OSTaskCreate( MyTask, sTask2, (void*)Stk2, 5 );
-	OSTaskCreate( MyTask, sTask3, (void*)Stk3, 6 );
-	OSTaskCreate( MyTask, sTask4, (void*)Stk4, 7 );
-	OSTaskCreate( MyTask, sTask5, (void*)Stk5, 8 );
+/*
+ Note  OS_STK_GROWTH is defined in OS_CPU.h
+ STACK parameter in OSTaskCreate is the top address
+ Also please take care of stack pointer usage in OSTaskStkInit/OS_CPU_C.C
+*/
+	OSTaskCreate( MyTask, sTask1, (void*)&Stk1[ OSMinStkSize() ], 4 );
+	OSTaskCreate( MyTask, sTask2, (void*)&Stk2[ OSMinStkSize() ], 5 );
+	OSTaskCreate( MyTask, sTask3, (void*)&Stk3[ OSMinStkSize() ], 6 );
+	OSTaskCreate( MyTask, sTask4, (void*)&Stk4[ OSMinStkSize() ], 7 );
+	OSTaskCreate( MyTask, sTask5, (void*)&Stk5[ OSMinStkSize() ], 8 );
 
     OSStart();
 }
